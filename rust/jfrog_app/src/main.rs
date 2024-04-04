3108a6e70c6331b81b3bf0d3bb3e8203b34d5a85
tomjfrog <tomj@jfrog.com>
2024-04-04 09:36:37 -0500

    Updated JFrog App for proper import of jfrog_quotes

--- rust/jfrog_app/src/main.rs
+++ rust/jfrog_app/src/main.rs
@@ -1,11 +1,11 @@
 // Import your library
-use yogi_quotes::get_random_quote;
+use jfrog_quotes::get_random_quote;
 
 fn main() {
     // Get a random quote
     let quote = get_random_quote();
 
     // Print the quote
-    println!("Random Yogi Berra quote: {}", quote);
+    println!("Random JFrog quote: {}", quote);
 }
 
